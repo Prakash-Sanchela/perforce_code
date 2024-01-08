485d1b064b40deed13adc617b09c900a1bafa00a
PRAKASH SANCHELA <38539199+Prakash-Sanchela@users.noreply.github.com>
2024-01-09 00:06:50 +0530

    Update visitor.cpp

--- src/visitor.cpp
+++ src/visitor.cpp
@@ -1,32 +1,4 @@
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <unordered_map>>
-#include <sstream>
-
-using namespace std;
-
-/*!*****************************************************************************
-* @brief 
-*   Time structure holds variables hour and minute
-*   basically a structure which is used to hold the time in hour & minute
-* 
-*******************************************************************************/
-struct Time {
-    int hour;
-    int minute;
-};
-
-/*!*****************************************************************************
-* @brief 
-*   VisitorEntry structure holds instance of Time structure
-*   which is used to hold the inTime and outTime of the visitors
-* 
-*******************************************************************************/
-struct VisitorEntry {
-    Time inTime;
-    Time outTime;
-};
+#include "visitor.h"
 
 /*!****************************************************************************
  * @brief 
